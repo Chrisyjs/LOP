5f1a376d49d91c599b17ab27cb18172227e7a63d
yujiashun <yujiashun@zhixuezhen.com>
2020-10-09 13:47:45 +0800

    feat：路由调整

--- ilop/src/router/index.js
+++ ilop/src/router/index.js
@@ -10,44 +10,44 @@ export default new VueRouter({
       path: '/login',
       component:() => import(/* webpackChunkName: "login" */ 'views/login.vue'),
     },
-    // {
-    //   path: '/claim',
-    //   redirect: '/claim/waitingList',
-    //   component:() => import(/* webpackChunkName: "claim_mainPage" */ 'views/claim/mainPage.vue'),
-    //   children: [
-    //     {
-    //       path: 'waitingList',
-    //       component:() => import(/* webpackChunkName: "claim_waitingList" */ 'views/claim/waitingList.vue'),
-    //     },
-    //     {
-    //       path: 'myList',
-    //       component:() => import(/* webpackChunkName: "claim_myList" */ 'views/claim/myList.vue'),
-    //     },
-    //     {
-    //       path: 'confirmClaim',
-    //       component:() => import(/* webpackChunkName: "claim_confirmClaim" */ 'views/claim/confirmClaim.vue'),
-    //     },
-    //     {
-    //       path: 'claimDetail',
-    //       component:() => import(/* webpackChunkName: "claim_claimDetail" */ 'views/claim/claimDetail.vue'),
-    //     },
-    //   ]
-    // },
     {
-      path: '/appointment',
-      component:() => import(/* webpackChunkName: "appointment_index" */ 'views/appointment/index.vue'),
+      path: '/claim',
+      redirect: '/claim/waitingList',
+      component:() => import(/* webpackChunkName: "claim_mainPage" */ 'views/claim/mainPage.vue'),
+      children: [
+        {
+          path: 'waitingList',
+          component:() => import(/* webpackChunkName: "claim_waitingList" */ 'views/claim/waitingList.vue'),
+        },
+        {
+          path: 'myList',
+          component:() => import(/* webpackChunkName: "claim_myList" */ 'views/claim/myList.vue'),
+        },
+        {
+          path: 'confirmClaim',
+          component:() => import(/* webpackChunkName: "claim_confirmClaim" */ 'views/claim/confirmClaim.vue'),
+        },
+        {
+          path: 'claimDetail',
+          component:() => import(/* webpackChunkName: "claim_claimDetail" */ 'views/claim/claimDetail.vue'),
+        },
+      ]
     },
-    // {
-    //   path: '/st',
-    //   component:() => import(/* webpackChunkName: "st_index" */ 'views/st/signUp/index.vue'),
-    // },
     {
-      path: '/myself',
-      component:() => import(/* webpackChunkName: "myself_index" */ 'views/myself/appointmentList/index.vue'),
+      path: '/st',
+      component:() => import(/* webpackChunkName: "st_index" */ 'views/st/signUp/index.vue'),
+    },
+    {
+      path: '/appointment/process',
+      component:() => import(/* webpackChunkName: "appointment_process" */ 'views/appointment/process/index.vue'),
+    },
+    {
+      path: '/appointment/list',
+      component:() => import(/* webpackChunkName: "appointment_list" */ 'views/appointment/list/index.vue'),
     },
     {
       path: '*',
-      redirect: '/appointment'
+      redirect: '/appointment/process'
     }
   ]
 })
