b2374b4495e171a2797c62bbe754c4c2288a3837
Chrisyjs <930136058@qq.com>
2020-10-18 21:37:54 +0800

    fix：端口号改变

--- rookie_card/views/main.js
+++ rookie_card/views/main.js
@@ -128,7 +128,7 @@ function submitTo() {
         leaveMsg: saySomething,
     }
     $.ajax({
-        url: 'http://www.landofpromise.co:8080/lop/app/freshman/add',
+        url: 'http://www.landofpromise.co:8088/lop/app/freshman/add',
         type: 'post',
         contentType: 'application/json;charset=UTF-8',
         dataType: 'json',
