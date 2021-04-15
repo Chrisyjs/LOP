de81903f68ba6174375764e5faf1207fafee2163
Chrisyjs <930136058@qq.com>
2021-04-15 23:24:55 +0800

    yjs

--- single_page/src/lib/config.js
+++ single_page/src/lib/config.js
@@ -1,6 +1,6 @@
 const domainConfig = {
-  signUp: 'http://www.landofpromise.co:8080/lop',
-  signUp2021: 'http://www.landofpromise.co:8080/lop',
+  signUp: 'http://www.landofpromise.co:8088/lop',
+  signUp2021: 'http://www.landofpromise.co:8088/lop',
   fruit: 'https://www.landofpromise.co:8088/lop'
 }
 
--- single_page/vue.config.js
+++ single_page/vue.config.js
@@ -1,7 +1,5 @@
 const { domainConfig } = require('./src/lib/config.js')
 const UglifyPlugin = require("uglifyjs-webpack-plugin");
-// const WebpackBundleAnalyzer = require('webpack-bundle-analyzer');
-const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 const path = require("path");
 const Timestamp = new Date().getTime();
 const isPro = process.env.NODE_ENV === "production" ? true : false;
@@ -11,61 +9,22 @@ const cdn = {
   js: [
     "https://cdn.bootcss.com/vue/2.6.10/vue.min.js",
     "https://cdn.bootcss.com/axios/0.19.0-beta.1/axios.min.js",
-    // "https://cdn.bootcss.com/vuex/3.1.1/vuex.min.js",
-    // "https://cdn.bootcss.com/vue-router/3.1.3/vue-router.min.js",
+    "https://cdn.bootcss.com/vuex/3.1.1/vuex.min.js",
+    "https://cdn.bootcss.com/vue-router/3.1.3/vue-router.min.js",
   ],
 };
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
-const pages = {};
-pageName === "signUp" &&
-  (pages.signUp = {
-    // 入口文件
-    entry: "./src/main.js" /*这个是根入口文件*/,
-    // 模板文件
-    template: "public/index.html",
-    // 输出文件
-    filename: "signUp.html",
-    // 页面title
-    title: "ST 报名",
-    chunks: ["chunk-vendors", "chunk-common", "signUp"],
-  });
-  pageName === "signUp2021" &&
-  (pages.signUp2021 = {
-    // 入口文件
-    entry: "./src/main.js" /*这个是根入口文件*/,
-    // 模板文件
-    template: "public/index.html",
-    // 输出文件
-    filename: "signUp2021.html",
-    // 页面title
-    title: "ST 报名",
-    chunks: ["chunk-vendors", "chunk-common", "signUp2021"],
-  });
-  pageName === "fruit" &&
-  (pages.fruit = {
-    // 入口文件
-    entry: "./src/main.js" /*这个是根入口文件*/,
-    // 模板文件
-    template: "public/index.html",
-    // 输出文件
-    filename: "fruit.html",
-    // 页面title
-    title: "ST 果子统计",
-    chunks: ["chunk-vendors", "chunk-common", "fruit"],
-  });
 module.exports = {
   lintOnSave: true,
   publicPath: "./",
   outputDir: "dist",
-  pages,
   devServer: {
     proxy: {
       // proxy all requests starting with /api to jsonplaceholder
       "/api": {
         target: domain, //代理接口
-        // target: 'http://192.168.0.248:8080/lop',   //代理接口
         changeOrigin: true,
         pathRewrite: {
           "^/api": "", //代理的路径
@@ -75,11 +34,11 @@ module.exports = {
   },
   chainWebpack: (config) => {
     if (isPro) {
-      // config.plugin('prefetch').tap(options => {
-      //   options[0].fileBlacklist = options[0].fileBlacklist || []
-      //   options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
-      //   return options
-      // })
+      config.plugin('prefetch').tap(options => {
+        options[0].fileBlacklist = options[0].fileBlacklist || []
+        options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
+        return options
+      })
       config.output.filename(`[name].[hash]${Timestamp}.js`).end();
       config.output.chunkFilename(`[name].[hash]${Timestamp}.js`).end();
       config.plugin("extract-css").tap((args) => [
@@ -98,45 +57,45 @@ module.exports = {
         });
         return args;
       });
-    /* config.plugin("html").tap((args) => {
+    config.plugin("html").tap((args) => {
       args[0].cdn = cdn;
       args[0].chunksSortMode = "none";
       return args;
-    }); */
+    });
+    config.plugin("html").tap((args) => {
+      args[0].pageName = pageName;
+      return args;
+    });
     config.resolve.alias.set("@", resolve("src"));
   },
   configureWebpack: (config) => {
-    let externals = {
-      vue: "Vue",
-      // vuex: "Vuex",
-      // "vue-router": "VueRouter",
-      Axios: "axios",
-    };
-    Object.assign(config, { externals });
-    // if (process.env.NODE_ENV === 'production') {
-    // 将每个依赖包打包成单独的js文件
-    let optimization = {
-      minimizer: [
-        new UglifyPlugin({
-          uglifyOptions: {
-            compress: {
-              // warnings: false,
-              drop_console: true, // console
-              drop_debugger: true,
-              pure_funcs: ["console.log"], // 移除console
+    if (isPro) {
+      let externals = {
+        vue: "Vue",
+        vuex: "Vuex",
+        "vue-router": "VueRouter",
+        Axios: "axios",
+      };
+      Object.assign(config, { externals });
+      // 将每个依赖包打包成单独的js文件
+      let optimization = {
+        minimizer: [
+          new UglifyPlugin({
+            uglifyOptions: {
+              compress: {
+                // warnings: false,
+                drop_console: true, // console
+                drop_debugger: true,
+                pure_funcs: ["console.log"], // 移除console
+              },
             },
-          },
-        }),
-      ],
-    };
-    Object.assign(config, {
-      optimization,
+          }),
+        ],
+      };
+      Object.assign(config, {
+        optimization,
     });
-    // config.plugins = config.plugins.concat([new WebpackBundleAnalyzer.BundleAnalyzerPlugin(), new LodashModuleReplacementPlugin()]);
-    config.plugins = config.plugins.concat([
-      new LodashModuleReplacementPlugin(),
-    ]);
-    // }
+    }
   },
   css: {
     loaderOptions: {
